c594e6b91d7f000597c8bd6496a9cf6c02e5b6bb
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2026-09-23 12:49:24 -0500

    Implement Basic In-Memory Index

--- idx/idx.ml
+++ idx/idx.ml
@@ -6,3 +6,18 @@ module type S = sig
   val create: unit -> t
   val lookup: t -> key -> rid list
 end
+
+module Basic : S = struct
+  module M = Map.Make(Int)
+
+  type key = int
+  type rid = int * int
+  type t = rid list M.t
+
+  let create () = M.empty
+
+  let lookup t key =
+    match M.find_opt key t with
+    | None -> []
+    | Some rids -> rids
+end
