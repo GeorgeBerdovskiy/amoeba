6660878943c90194caf95fbcc08ef5279f5db464
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2026-09-24 15:40:51 -0500

    Improve Page Insert Logic

--- page/page.ml
+++ page/page.ml
@@ -14,22 +14,37 @@ module Basic (R : Record.S) : S with type record = R.t = struct
   type record = R.t
   type slot = int
 
+  (* How many bytes does a field communicating offset take? *)
+  let offset_field_size = 2
   let create size = Bytes.create size
   let is_dirty _ = false
 
+  (* Byte offset of the next available slot. *)
+  let slot_end_offset page = Bytes.get_uint16_ne page 0
+
+  (* Byte offset of the last inserted record.  *)
+  let record_start_offset page = Bytes.get_uint16_ne page 2
+
+  (* How many bytes of space are available for records *)
+  let available_space page =
+    record_start_offset page - slot_end_offset page - offset_field_size
+
+  let reserve page size =
+    if available_space page >= size then
+      let new_slot_offset = slot_end_offset page + 2 in
+      let new_record_offset = record_start_offset page - size in
+      let _ = Bytes.set_int16_ne page 0 new_slot_offset in
+      let _ = Bytes.set_int16_ne page 2 new_record_offset in
+      Some (new_slot_offset, new_record_offset)
+    else None
+
   let insert page record =
-    let slot_end = Bytes.get_uint16_ne page 0 in
-    let record_start = Bytes.get_uint16_ne page 2 in
-    let space = record_start - slot_end - 16 in
     let record_size = R.size record in
-    if record_size > space then None
-    else
-      let offset = record_start - record_size in
-      let _ = Bytes.blit (R.to_bytes record) 0 page offset record_size in
-      let _ = Bytes.set_int16_ne page slot_end offset in
-      let _ = Bytes.set_int16_ne page 0 (slot_end + 16) in
-      let _ = Bytes.set_int16_ne page 2 offset in
-      Some slot_end
+    match reserve page record_size with
+    | Some (slot, offset) ->
+        let _ = Bytes.blit (R.to_bytes record) 0 page offset record_size in
+        Some slot
+    | None -> None
 
   let get _ = None
 end
